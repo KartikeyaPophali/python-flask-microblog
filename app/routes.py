40557fe53a7f3df3fea453497b4f96eac059ccb7
Kartikeya Pophali <kartikeyapophali@gmail.com>
2022-01-29 16:58:18 +0530

    Introduce Jinja templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {"username": "Kartikeya"}
+    posts = [
+        {
+            'author': {'username': 'Raj'},
+            'body': 'Learning the Flask framework in Python for web development!'
+        },
+        {
+            'author': {'username': 'Sid'},
+            'body': 'Hope the world gets out of this pandemic soon.'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
